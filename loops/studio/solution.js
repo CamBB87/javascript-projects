474ad39f47991094e1ad4c1dd062ff4e7f24e2cd
Cameron Beebe <cameron.beebe87@yahoo.com>
2023-11-04 16:52:57 -0500

    mmmmm

--- loops/studio/solution.js
+++ loops/studio/solution.js
@@ -15,25 +15,24 @@ function mealAssembly(protein, grains, veggies, beverages, desserts, numMeals) {
   let meal = [];
   /// Part A #2: Write a ``for`` loop inside this function
   /// Code your solution for part A #2 below this comment (and above the return statement) ... ///
- for (let i = 0; i < protein.length; i++) {
-  meal = [];
-  meal.push(pantry[i][i]);
-  for (let j = 0; j < protein.length; j++) {
-    meal.push(pantry)
-    meals.push(meal)
-  }
-  
- }
-
+  for (let i = 0; i < numMeals; i++) {
+    for (let j = 0; j < pantry.length; j++) {
+      meal.push(pantry[j][i]);
+    };
+    meals.push(meal);
+    meal = [];
+  };
   return meals;
-}
+};
 
 
 function askForNumber() {
-  numMeals = input.question("How many meals would you like to make?");
-  
-  /// CODE YOUR SOLUTION TO PART B here ///
+  numMeals = input.question("How many meals would you like to make?: ");
 
+  /// CODE YOUR SOLUTION TO PART B here ///
+  while (numMeals <= 0 || numMeals > 6) {
+    numMeals = input.question(`how many?? we can't do that here (enter 1 - 6): `)
+  }
   return numMeals;
 }
 
@@ -47,24 +46,24 @@ function generatePassword(string1, string2) {
 }
 
 function runProgram() {
-  
+
   /// TEST PART A #2 HERE ///
   /// UNCOMMENT the two lines of code below that invoke the mealAssembly function (starting with 'let meals =') and print the result ///
   /// Change the final input variable (aka numMeals) here to ensure your solution makes the right number of meals ///
   /// We've started with the number 2 for now. Does your solution still work if you change this value? ///
-  
+
   let meals = mealAssembly(protein, grains, veggies, beverages, desserts, 2);
   console.log(meals)
-  
+
 
   /// TEST PART B HERE ///
   /// UNCOMMENT the next two lines to test your ``askForNumber`` solution ///
   /// Tip - don't test this part until you're happy with your solution to part A #2 ///
-  
-  // let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
-  // console.log(mealsForX);
 
-    /// TEST PART C HERE ///
+  let mealsForX = mealAssembly(protein, grains, veggies, beverages, desserts, askForNumber());
+  console.log(mealsForX);
+
+  /// TEST PART C HERE ///
   /// UNCOMMENT the remaining commented lines and change the password1 and password2 strings to ensure your code is doing its job ///
 
   // let password1 = '';
